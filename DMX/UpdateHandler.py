f0dacee15fb99b341068c7a169d908fdfce811c0
danieljpietz <danieljpietz@gmail.com>
2020-11-09 16:23:28 -0700

    Fixed windows issue

--- DMX/UpdateHandler.py
+++ DMX/UpdateHandler.py
@@ -1,6 +1,6 @@
 import threading as threading
 import time as tm
-
+import os
 import pyenttec as dmx
 
 
@@ -11,7 +11,13 @@ class UpdateHandler(object):
         self.shouldUpdate = True
         self.updateThread = threading.Thread(target=self.update)
         self.updateID = -1
-        self.port = dmx.select_port()
+        if os.name == 'nt':
+            self.port = 0 * [512]
+            pass
+        else:
+            self.port = dmx.select_port(0)
+            pass  # other (unix)
+
 
     def begin(self):
         self.updateThread.start()
@@ -22,7 +28,8 @@ class UpdateHandler(object):
         while self.shouldUpdate:
             self.updateID = 1 + self.updateID
             self.window.updateDMX()
-            self.port.render()
+            if os.name != 'nt':
+                self.port.render()
             drift = (tm.time() - masterClock) - self.updateID / self.refreshRate
             tm.sleep(1 / self.refreshRate - drift * (drift < 1 / self.refreshRate))
         pass
